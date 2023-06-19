e345c4a2b7be4b8b1cac1824ac705a51b329dc3a
Alberto Lazari <alberto.lazari@gmail.com>
2023-06-19 12:42:45 +0200

    MM: numbered lectures

--- mobile-programming/get-lectures.sh
+++ mobile-programming/get-lectures.sh
@@ -7,11 +7,13 @@ course_link='https://stem.elearning.unipd.it/course/view.php?id=5309'
 file_link_regex='https://stem.elearning.unipd.it/mod/resource/view.php?id=[0-9]*'
 links=$(curl -s $course_link --cookie "$cookie" | grep -o $file_link_regex)
 
+i=1
 for link in $links; do
     redirect=$(curl -s --cookie "$cookie" $link | grep -o 'https.*\.pdf')
     if [[ ! $redirect =~ .*x4\.pdf ]]; then
-        file=$(echo ${redirect/https:*\/content\/[0-9]*\/})
+        file=$(echo $(printf %02d $i)-${redirect/https:*\/content\/[0-9]*\/})
         echo Downloading $file
         curl -s --cookie "$cookie" $redirect -o $file
+        i=$(( i + 1 ))
     fi
 done
