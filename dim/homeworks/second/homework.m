6b9555afd61518bb97c9268fcbea85cb64ee6b8a
Alberto Lazari <alberto.lazari@gmail.com>
2023-11-28 16:13:53 +0100

    Semi-working

--- dim/homeworks/second/homework.m
+++ dim/homeworks/second/homework.m
@@ -8,4 +8,16 @@ LFMatlabPathSetup
 LFUtilProcessWhiteImages
 
 % Decode Images
-LFUtilDecodeLytroFolder
+% DecodeOptions.OptionalTasks = 'Rectify'
+% Don't use color correction, it's terrible
+% DecodeOptions.OptionalTasks = 'ColourCorrect'
+% LFUtilDecodeLytroFolder([], [], DecodeOptions)
+LFUtilDecodeLytroFolder()
+
+load('Images/IMG_0751__Decoded.mat', 'LF');
+% LFDisp(LF)
+RenderOptions.PathRadius_percent = 50;
+RenderOptions.FrameDelay = 1/25;
+LFDispVidCirc(LF, RenderOptions)
+% LFDispMousePan(LF)
+% LFDispLawnmower(LF)
