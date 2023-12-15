a9f51e7e595ec45dad892e0130623e6059ae7bfb
Alberto Lazari <alberto.lazari@gmail.com>
2023-12-15 19:57:38 +0100

    Better print

--- dim/labs/lab-06/compression.m
+++ dim/labs/lab-06/compression.m
@@ -1,4 +1,5 @@
 close all
+clc
 
 im = im2double(imread('original/IMG15.png'));
 
@@ -8,10 +9,26 @@ imwrite(im, 'compressed/I15_JPEG_50.jpg', 'Quality', 50);
 imwrite(im, 'compressed/I15_JPEG_25.jpg', 'Quality', 25);
 
 % JPEG 2000
-imwrite(im, 'compressed/I15_JPEG2K_1.25.jp2', 'CompressionRatio', 1.25);
+imwrite(im, 'compressed/I15_JPEG2K_125.jp2', 'CompressionRatio', 1.25);
 imwrite(im, 'compressed/I15_JPEG2K_2.jp2', 'CompressionRatio', 2);
-imwrite(im, 'compressed/I15_JPEG2K_2.5.jp2', 'CompressionRatio', 2.5);
+imwrite(im, 'compressed/I15_JPEG2K_25.jp2', 'CompressionRatio', 2.5);
 
-figure;
-imshowpair(im, im2double(imread('compressed/I15_JPEG_25.jpg')), 'montage');
-title('Original vs JPEG 25');
+% Compare results
+% figure;
+% imshowpair(im, im2double(imread('compressed/I15_JPEG_25.jpg')), 'montage');
+% title('Original vs JPEG 25');
+
+images = {
+    'I15_JPEG_75' im2double(imread('compressed/I15_JPEG_75.jpg'))
+    'I15_JPEG_50' im2double(imread('compressed/I15_JPEG_50.jpg'))
+    'I15_JPEG_25' im2double(imread('compressed/I15_JPEG_25.jpg'))
+    'I15_JPEG2K_125' im2double(imread('compressed/I15_JPEG2K_125.jp2'))
+    'I15_JPEG2K_2' im2double(imread('compressed/I15_JPEG2K_2.jp2'))
+    'I15_JPEG2K_25' im2double(imread('compressed/I15_JPEG2K_25.jp2'))
+};
+
+fprintf('Image\t\tPSNR\t\tSSIM\t\tBRIQUE\t\tNIQE\n')
+for i = 1:6
+    image = images{i, 2};
+    fprintf("%s\t%f\t%f\t%f\t%f\n", images{i, 1}, psnr(image, im), ssim(image, im), brisque(image), niqe(image))
+end
